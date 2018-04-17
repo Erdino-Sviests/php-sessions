5fc708caceebf337dfd007092d739971d3c9630c
Niklavs Emils Eglitis <34540352+NiklavsE@users.noreply.github.com>
2018-04-17 14:04:04 +0300

    Update verification.php

--- verification.php
+++ verification.php
@@ -1,7 +1,7 @@
 <?php 
 session_start(); 
-$id = $_SESSION['id'];
-$funkcija = null ; 
+$id = $_GET['id'];
+$token = $_GET['token']; 
 include 'db.php' ; 
 ?>
 <html> 
@@ -14,15 +14,23 @@ include 'db.php' ;
 <?php
 $prompt = ''; 
 if ($_POST['Go']) { 
+if(isset($_GET['id']) && isset($_GET['token'])) { 
 	if 
-	 (get_token($id) != null ) 
+	 (activate_account($id, $token) == true) 
 	{ 
 	echo "<script>alert('The verification was succesful');document.location.href = 'profile.php'</script>"; // if succesful
-} 
-else { 
+	}
+	else 
+	{ 
+	$prompt = 'Verification unsuccesful'; 
+	echo "<script>alert('The verification was unsuccesful');document.location.href = ' ' </script>"; // if failed
+	}
+}
+else 
+{
 $prompt = 'Verification unsuccesful'; 
 echo "<script>alert('The verification was unsuccesful');document.location.href = ' ' </script>"; // if failed
-}
+} 
 }
 ?>
 </html>
