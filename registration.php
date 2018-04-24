037163bb56e6cc14149ec0c01d802e8b15a2730c
Erdino-Sviests <38459653+Erdino-Sviests@users.noreply.github.com>
2018-04-24 14:03:34 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,13 +1,15 @@
 <?php
 include 'db.php';
-if (isset($_Post['submit'])) { //Pārbauda vai nospiest poga, lai ievadītu datus
+$succes = "";
+$error  = "";
+if (isset($_POST['submit'])) { //Pārbauda vai nospiest poga, lai ievadītu datus
     $succes = "";
     $error  = "";
-    if ($_Post['id'] != "") { //Pārbauda vai lietotājvārds nav tukš.
-        if ($_Post['password1'] != "") { //Pārbauda vai parole nav tukša
-            if ($_Post['password2'] != "") { //Pārbauda vai atkartotā parole nav tukša
-                if ($_Post['password1'] == $_Post['password2']) { //Salīdzina paroli ar atkārtoto paroli
-                    register($_Post['id'], $_Post['password1']); //Izsauc funkciju, kas ieraksta datu bāzē lietotajvārdu un paroli
+    if ($_POST['id'] != "") { //Pārbauda vai lietotājvārds nav tukš.
+        if ($_POST['password1'] != "") { //Pārbauda vai parole nav tukša
+            if ($_POST['password2'] != "") { //Pārbauda vai atkartotā parole nav tukša
+                if ($_POST['password1'] == $_POST['password2']) { //Salīdzina paroli ar atkārtoto paroli
+                    register($_POST['id'], $_POST['password1']); //Izsauc funkciju, kas ieraksta datu bāzē lietotajvārdu un paroli
                     $succes = "Registration has been successful. You must verify your registration by activating link which we have sent to your email."; //Izvada virs formas, ka reģistrācija ir notikus. Izvada tajā pašā vietā, kur error(Vismaz orģinālaja kodā)
                 } else {
                     $error = "Passwords  do not match!";
@@ -24,15 +26,27 @@ if (isset($_Post['submit'])) { //Pārbauda vai nospiest poga, lai ievadītu datu
 } //Tālāk seko html formas paraugs (nev tas, ko izmantosim)
 ?>
 <html lang="en">  
-<body>
-    <h1>Register</h1>
+<body style="
+    display: flex;
+	background-color:#E7EAF4;
+">
+    <form id="regform" method="post" action="" style="
+    margin: auto;
+    height:250px;
+    width:275px;
+    text-align: center;
+    background-color: lightgrey;
+    box-shadow:0px 0px 0px 8px black inset;
+    "
+>
+	    <h1>Register</h1>
     <p><?php
-echo $error;
-?></p>
-    <form id="regform" method="post" action="">
-        <p>Username:<input type="text" name="id" required="required" /></p>
-        <p>Password:<input type="password" name="password1" required="required" /></p>
-        <p>Password:<input type="password" name="password2" required="required" /></p>
+	echo $error;
+	echo $succes;
+	?></p>
+        <p>Username:<input type="text" name="id" required="required" style=" background-color:#69E1AD"/></p>
+        <p>Password:<input type="password" name="password1" required="required" style=" background-color:#69E1AD" /></p>
+        <p>Password:<input type="password" name="password2" required="required" style=" background-color:#69E1AD"/></p>
         <input type="submit" name="submit" value="Login"/>
     </form>
 </body>
